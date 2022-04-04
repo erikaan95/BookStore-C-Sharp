512547b477fd0937983a941b4ec6fa9708856d38
erikaan <ancoraerika@gmail.com>
2022-04-04 22:25:05 +0200

    Aggiungere i file di progetto.

---
+++ BookStore C Sharp/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("BookStore C#");
+
+// Info book - dettagli prodotto //
+string Titolo = "Clean Code";
+Titolo = "Clean code.Guida per diventare bravi artigiani nello sviluppo agile di software";
+Console.WriteLine("Il titolo del libro è: " + Titolo);
+
+string Autore = "Robert C.Martin";
+Console.WriteLine("L’autore è: " + Autore);
+
+double ISBN;
+ISBN = (8850334389);
+Console.WriteLine("ISBN: " + ISBN);
+
+int numeroPagine;
+numeroPagine = (435);
+Console.WriteLine("Numero pagine: " + numeroPagine);
+
+int pesoLibro;
+pesoLibro = (860);
+Console.WriteLine("Peso del libro è: " + pesoLibro + "gr");
+
+string Dimensioni;
+Dimensioni = "Le dimensioni del libro sono: ";
+
+Console.WriteLine(Dimensioni);
+float Larghezza;
+Larghezza = ((float)17.3);
+Console.WriteLine("Larghezza " + Larghezza + "cm");
+
+float Altezza;
+Altezza = ((float)2.6);
+Console.WriteLine("Altezza " + Altezza + "cm");
+
+float Profondità;
+Profondità = ((float)17.2);
+Console.WriteLine("Profondità " + Profondità + "cm");
+
+// Rece //
+float valutazioneMedia;
+valutazioneMedia = ((float)4.4);
+Console.WriteLine("La valutazione media è di: " + valutazioneMedia + "stelle su 5");
+
+string Kindle;
+Kindle = ("Disponibile");
+Console.WriteLine("Versione Kindle: " + Kindle);
+float prezzoKindle;
+prezzoKindle = ((float)12.99);
+Console.WriteLine("Prezzo Kindle: " + prezzoKindle + "€");
+
+string copertinaFlessibile;
+copertinaFlessibile = ("Disponibile");
+Console.WriteLine("La copertina flessibile è: " + copertinaFlessibile);
+float prezzoCopertinaFlessibile;
+prezzoCopertinaFlessibile = ((float)37.90);
+Console.WriteLine("Prezzo copertina flessibile: " + prezzoCopertinaFlessibile + "€");
